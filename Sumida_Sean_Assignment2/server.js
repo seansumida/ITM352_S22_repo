3c7683fdb268ffaae700f985e27e3aaf3fe70356
seansumida <60155464+seansumida@users.noreply.github.com>
2022-04-14 03:13:44 -1000

    still need to clean up

--- Sumida_Sean_Assignment2/server.js
+++ Sumida_Sean_Assignment2/server.js
@@ -35,7 +35,15 @@ app.get("/product_data.js", function (request, response, next) {
    var products = `var products_array = ${JSON.stringify(products_array)};`;
    response.send(products);
 });
+/*
 
+██████╗░██╗░░░██╗██████╗░░█████╗░██╗░░██╗░█████╗░░██████╗███████╗
+██╔══██╗██║░░░██║██╔══██╗██╔══██╗██║░░██║██╔══██╗██╔════╝██╔════╝
+██████╔╝██║░░░██║██████╔╝██║░░╚═╝███████║███████║╚█████╗░█████╗░░
+██╔═══╝░██║░░░██║██╔══██╗██║░░██╗██╔══██║██╔══██║░╚═══██╗██╔══╝░░
+██║░░░░░╚██████╔╝██║░░██║╚█████╔╝██║░░██║██║░░██║██████╔╝███████╗
+╚═╝░░░░░░╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝
+*/
 // Process purchase request
 app.post('/purchase', function (request, response, next) {
    // Variables used for validation
@@ -70,7 +78,7 @@ app.post('/purchase', function (request, response, next) {
       for (i in products_array) {
           products_array[i].quantity -= Number(request.body['quantity' + i]);
       }
-      // Goes to invoice upon valid purchase
+      // Goes to login upon valid purchase
       response.redirect("./login.html?" + qString); 
   }
    else {
@@ -81,13 +89,24 @@ app.post('/purchase', function (request, response, next) {
        response.redirect(`./products_display.html?errorMessage=${err_msg}&` + qString); 
    }
 });
+/*
 
+██╗░░░░░░█████╗░░██████╗░██╗███╗░░██╗
+██║░░░░░██╔══██╗██╔════╝░██║████╗░██║
+██║░░░░░██║░░██║██║░░██╗░██║██╔██╗██║
+██║░░░░░██║░░██║██║░░╚██╗██║██║╚████║
+███████╗╚█████╔╝╚██████╔╝██║██║░╚███║
+╚══════╝░╚════╝░░╚═════╝░╚═╝╚═╝░░╚══╝
+*/
 // process login modified from lab 14 ex 3
 app.post("/login", function (request, response) {
     var login_error = '';
     if (typeof user_data_parsed[request.body['email'].toLowerCase()] != 'undefined'){
         if(user_data_parsed[request.body.email].password == request.body.password){
-            response.redirect('./invoice.html?' + qString);
+            request.query.name = user_data_parsed[request.body['email'].toLowerCase()].name;
+            request.query.email = request.body['email'].toLowerCase();
+            let current_user = qs.stringify(request.query);
+            response.redirect('./invoice.html?' + qString + '&' + current_user);
             return;
         }
         else{
@@ -99,66 +118,169 @@ app.post("/login", function (request, response) {
     } 
     response.redirect(`./login.html?email=${request.body['email'].toLowerCase()}&errors=` + login_error)
 });
+/*
 
-// processes registrations adapted from lab 14 ex 4
+██████╗░███████╗░██████╗░██╗░██████╗████████╗███████╗██████╗░
+██╔══██╗██╔════╝██╔════╝░██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+██████╔╝█████╗░░██║░░██╗░██║╚█████╗░░░░██║░░░█████╗░░██████╔╝
+██╔══██╗██╔══╝░░██║░░╚██╗██║░╚═══██╗░░░██║░░░██╔══╝░░██╔══██╗
+██║░░██║███████╗╚██████╔╝██║██████╔╝░░░██║░░░███████╗██║░░██║
+╚═╝░░╚═╝╚══════╝░╚═════╝░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝
+*/
+// processes registrations adapted from lab 14 ex 4 and Tiffany Young
 app.post("/register", function(request, response){
+    //creates errors message to later display in the innerHTML
     var errors_reg = {};
     errors_reg['name'] = [];
     errors_reg['email'] = [];
     errors_reg['password'] = [];
     errors_reg['repeat_password'] = [];
-    if (/^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,3}$/.test(request.body.email) == false) {
-        errors_reg['email'].push(`Please enter a valid email`);
-     } else if (request.body['email'].toLowerCase().length == 0) {
-        errors_reg['email'] = `Enter an email`;
+
+    // checks if name is using correct characters
+    if (/^[A-Za-z, ]+ [A-Za-z]+$/.test(request.body['name'])) {
+    // checks if name is input correctly   
+    } 
+    else {
+    errors_reg['name'].push(`Enter your full name (ex. Dan Port)`);
+    }
+    // checks if name is less than 30 characters
+    if (request.body['name'].length > 30) {
+        errors_reg['name'].push(`Please enter your name with less than 30 characters`);
+    }
+
+    // email limitations used from w3 schools
+    if (/^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,3}$/.test(request.body.email)) {
+       // checks if there is a email entered
+     } 
+     else {
+        errors_reg['email'].push(`Enter a valid email (ex. dport@hawaii.edu)`);
      }
-        //check if email is unique
-   if (typeof user_data_parsed[request.body['email'].toLowerCase()] != 'undefined') {
-    errors_reg['email'] = `This email has already been registered`;
- }
+        // checks if email is alreaady in use
+    if (typeof user_data_parsed[request.body['email'].toLowerCase()] != 'undefined') {
+        errors_reg['email'].push(`This email has already in use`);
+    }
 
- //check password > 8 
- if (request.body.password.length < 8) {
-    errors_reg['password'] = `Minimum 8 characters`;
- } else if (request.body.password.length == 0) { //nothing entered
-    errors_reg['password'] = `Enter a password`;
- }
+    // checks if a password is entered
+    if (request.body.password == "") { 
+        errors_reg['password'].push(`Enter a password`);
+      // checks if password has atleast 8 characters
+    } else if (request.body.password.length < 8) {
+        errors_reg['password'].push(`Please enter a password with a minimum of 8 characters`);
+    } 
 
- //check repeated password for matches
- if (request.body['password'] != request.body['repeat_password']) {
-    errors_reg['repeat_password'] = `The passwords do not match`;
- }
+    // checks if password is re-entered
+    if (request.body.password == "") { 
+        errors_reg['repeat_password'].push(`Re-enter your password`);
+      // checks if the re-entered password matched
+    } else if (request.body['password'] != request.body['repeat_password']) {
+        errors_reg['repeat_password'].push(`Your passwords do not match`);
+    }
+    var amount_errors = 0;
+    for (errors in errors_reg) {
+        amount_errors += errors_reg[errors].length;
+    }
+// from assignment 2 code examples and adapted from Tiffany Young
+// saves the new registred users data to user_data json file
+ if (amount_errors == 0) {
+    console.log('no errors in registration')
 
- //full name validation
- if (/^[A-Za-z, ]+$/.test(request.body['name'])) {
-    //check if the name is correct   
- } else {
-    errors_reg['name'] = `Please enter your full name`;
- }
- //check if name is less than 30 characters
- if (request.body['name'].length > 30) {
-    errors_reg['name'] = `Please enter less than 30 characters`;
- }
-
- //assignment 2 code examples
- //save new registration data to user_data.json
- if (Object.keys(errors_reg).length == 0) {
-    console.log('no registration errors')//store user data in json file
+    // stores the users info in the user_data json file
     user_data_parsed[request.body['email'].toLowerCase()] = {};
-    user_data_parsed[request.body['email'].toLowerCase()].password = request.body.password;
     user_data_parsed[request.body['email'].toLowerCase()].name = request.body.name;
-
-    fs.writeFileSync(filename, JSON.stringify(user_data_parsed), "utf-8");
-
-    qty_data_obj['email'] = request.body['email'].toLowerCase();
-    qty_data_obj['name'] = user_data_parsed[request.body['email'].toLowerCase()]['name'];
-    response.redirect('./invoice.html?' + qString); //all good! => to invoice w/data
- } else {
-     request.body.errors_obj = JSON.stringify(errors_reg)
-    request.body['errors_reg'] = JSON.stringify(errors_reg);
+    user_data_parsed[request.body['email'].toLowerCase()].password = request.body.password;
+    // writes the users info in the the user_data json file
+    fs.writeFileSync(user_data, JSON.stringify(user_data_parsed), "utf-8");
+    // addes name and email to query
+    request.query.name = user_data_parsed[request.body['email'].toLowerCase()].name;
+    request.query.email = request.body['email'].toLowerCase();
+    let current_user = qs.stringify(request.query);
+    // goes to invoice 
+    response.redirect('./invoice.html?' + qString + '&' + current_user); 
+ } 
+ else {
+    console.log('errors in registration')
+    request.body.errors_reg = JSON.stringify(errors_reg)
     response.redirect('./register.html?' + qs.stringify(request.body));
  }
 });
+/* 
+
+███████╗██████╗░██╗████████╗██╗███╗░░██╗░██████╗░
+██╔════╝██╔══██╗██║╚══██╔══╝██║████╗░██║██╔════╝░
+█████╗░░██║░░██║██║░░░██║░░░██║██╔██╗██║██║░░██╗░
+██╔══╝░░██║░░██║██║░░░██║░░░██║██║╚████║██║░░╚██╗
+███████╗██████╔╝██║░░░██║░░░██║██║░╚███║╚██████╔╝
+╚══════╝╚═════╝░╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░
+*/
+// processing editing page that allows the user to change their data
+// adapted from Tiffany Young
+app.post("/editing", function (request, response) { 
+    var edited_errors = {};
+    edited_errors['email'] = [];
+    edited_errors['password'] = [];
+    edited_errors['new_password'] = [];
+    edited_errors['repeat_new_password'] = [];
+    // email limitations used from w3 schools
+    if (/^[a-zA-Z0-9._]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,3}$/.test(request.body.email)) {
+        // checks if there is a email entered
+    } 
+    else {
+         edited_errors['email'].push(`Enter a valid email (ex. dport@hawaii.edu)`);
+    }
+    // checks if there is a registered account with the input email
+    if (typeof user_data_parsed[request.body['email'].toLowerCase()] != 'undefined') {
+        // checks if the password for the account is correct
+        if (user_data_parsed[request.body['email'].toLowerCase()].password == request.body['password']) {
+            // checks if password has atleast 8 characters
+            if (request.body.new_password.length < 8) {
+                edited_errors['new_password'].push('Passwords must have a minimum of 8 characters.');
+            }
+            // checks for the correct password for the account
+            if (user_data_parsed[request.body['email'].toLowerCase()].password != request.body['password']) {
+                edited_errors['password'].push('Incorrect password');
+            }
+            // checks both new passwords are the same
+            if (request.body.new_password != request.body.repeat_new_password) {
+                edited_errors['repeat_new_password'].push('Both passwords must match');
+            }
+            // checks if new password is not the same as the old password
+            if (request.body.new_password  == request.body['password']) {
+                edited_errors['new_password'].push(`New password cannot be the same as the old password`);
+            }
+        } 
+        // input email is incorrect
+        else { edited_errors['password'].push(`Incorrect Password`);}
+    } 
+    // no emails are accounts are registered with input email
+    else { edited_errors['email'].push(`Email has not been registered`);}
+
+    var amount_errors = 0;
+    for (errors in edited_errors) {
+        amount_errors += edited_errors[errors].length;
+    }
+    if (amount_errors == 0) {
+        console.log('no errors with changing passwords')
+
+       // stores the users new password in the user_data json file
+        user_data_parsed[request.body['email'].toLowerCase()].password = request.body.new_password
+ 
+        // writes the changed password info in the the user_data json file
+        fs.writeFileSync(user_data, JSON.stringify(user_data_parsed), "utf-8");
+        // addes name and email to query
+        request.query.name = user_data_parsed[request.body['email'].toLowerCase()].name;
+        request.query.email = request.body['email'].toLowerCase();
+        let current_user = qs.stringify(request.query);
+        // goes to invoice 
+        var confirmation = 'Password successfully changed!'
+        response.redirect('./login.html?' + qString + '&' + current_user + '&errors=' + confirmation); 
+        return;
+    } 
+    else {
+        console.log('errors with changing passwords')
+        request.body['edited_errors'] = JSON.stringify(edited_errors);
+        response.redirect('./editing.html?' + qs.stringify(request.body));
+    }
+ });
 
 // Borrowed and modified from Lab 13 ex 5
 function isNonNegInt(q, returnErrors = false) {
